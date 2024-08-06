ec2f2bbd09679a57f61f723613e97f8a34ac9597
without-eyes <dimka180205@gmail.com>
2024-08-06 15:45:15 +0300

    Added planning comments and made "ping" files

---
+++ include/ping.h
@@ -0,0 +1,8 @@
+#ifndef PING_PING_H
+#define PING_PING_H
+
+void ping(char* userInput);
+
+void isIpAddress(char* userInput);
+
+#endif //PING_PING_H
---
+++ src/ping.c
@@ -0,0 +1,20 @@
+#include "../include/ping.h"
+
+void ping(char* userInput) {
+    // convert site name into ip
+
+
+    // send packets(loop of 4(or maybe more) echo requests)
+    // mark the time
+    // if:
+    // 1) host available - receive respond;
+    // 2 )host offline or doesn't accept icmp - no reply, inform
+    // that it can be indication of network problem;
+    // 3) packet doesn't reach the host within certain time -
+    // inform of packet loss
+    // print message
+}
+
+void isIpAddress(char* userInput) {
+
+}
